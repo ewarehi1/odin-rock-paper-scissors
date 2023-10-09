7910c5bbf852800eaa3dd67a4d5d343e64062182
ewarehi1 <128170875+ewarehi1@users.noreply.github.com>
2023-10-09 14:07:50 -0600

    Write psuedocode

---
+++ rock-paper-scissors.js
@@ -0,0 +1,6 @@
+// PSUEDOCODE:
+// Establish the three choices (rock, paper, or scissors)
+// When user inputs one of three choices 
+// Script randomly select one of the three choices
+// Then compares the inputs
+// Outputs one of three responses (win, lose, or tie)
