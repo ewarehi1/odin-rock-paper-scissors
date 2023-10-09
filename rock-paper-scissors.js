1ed8c5c39d63014fa4a6e59dea6b101c0f82963d
ewarehi1 <128170875+ewarehi1@users.noreply.github.com>
2023-10-09 14:44:27 -0600

    Create function for random computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,3 +5,18 @@
 // Compares the inputs and selects one of three result (win, lose, or tie)
 // Return userChoice, opponentChoice, and result
 
+// 1. Script randomly select one of the three choices
+let getComputerChoice = function() {
+    let randNumber = Math.floor(Math.random() * 3);
+    let randChoice;
+
+    if (randNumber === 0) {
+        randChoice = "rock";
+    } else if (randNumber === 1) {
+        randChoice = "paper";
+    } else {
+        randChoice = "scissors";
+    };
+
+    return randChoice;
+};
